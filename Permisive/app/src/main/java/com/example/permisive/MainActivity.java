8a6a4818e67d0aec00c405411b29b236163f99cc
Unknown <j.urena202@live.com>
2020-10-12 09:56:38 -0400

    Permisive

--- Permisive/app/src/main/java/com/example/permisive/MainActivity.java
+++ Permisive/app/src/main/java/com/example/permisive/MainActivity.java
@@ -1,11 +1,7 @@
-package com.example.myfirstapp;
+package com.example.permisive;
 
-import android.widget.ArrayAdapter;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,8 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
-
-
 }
