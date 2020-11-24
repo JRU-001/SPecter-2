b20a0c8929223962c95a1cf06f7c966d11f30ca7
Unknown <j.urena202@live.com>
2020-11-24 10:42:51 -0400

    partially

---
+++ Commerce-E/app/src/main/java/com/example/commerce_e/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.example.commerce_e;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
